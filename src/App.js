2b9c3a6afe172a4ceaa27e2df31ee6ade6abef6a
Kornnythz <vatcharakon.k@gmail.com>
2021-12-04 10:34:53 +0700

    hooks effect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Logo from './components/Logo';
+import Menu from './components/Menu';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Logo/>
       <Header/>
       <Footer title="Facebook" website="www.facebook.com" address="Bangkok" postcode={10210} status={true} />
+      <Sidebar/>
+      <Menu/>
     </div>
   );
 }
